66ec323d72322a58e14060fbc5b489a54c5b6940
Joeseph McCann <joesephmccann@Joesephs-MacBook-Air.local>
2021-01-12 22:20:49 -0600

    set up first api route

--- pages/index.js
+++ pages/index.js
@@ -1,7 +1,15 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
 
+import useSWR from 'swr';
+
+const fetcher = (url) => fetch(url).then((res) => res.json());
+
 export default function Home() {
+  const { data, error } = useSWR('/api/hello', fetcher);
+
+  if (error) return <div>failed to load</div>;
+  if (!data) return <div>loading...</div>;
   return (
     <div className={styles.container}>
       <Head>
@@ -11,7 +19,7 @@ export default function Home() {
 
       <main className={styles.main}>
         <h1 className={styles.title}>
-          Hello World
+          {data.name}
         </h1>
       </main>
 
