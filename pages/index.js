48781e8129d898055e43d00612c9dc723ae65c58
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-01-27 10:40:24 -0600

    updated title in Head to Yelp Camp

--- pages/index.js
+++ pages/index.js
@@ -14,7 +14,7 @@ export default function Home() {
 	return (
 		<div className={styles.container}>
 			<Head>
-				<title>Create Next App</title>
+				<title>Yelp Camp</title>
 				<link rel="icon" href="/favicon.ico" />
 			</Head>
 
