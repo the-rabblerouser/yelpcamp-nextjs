e4740ec0ad1523555d498e0c9b6dd24efa7c51ab
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-02-04 12:49:28 -0600

    created a delete button in /campground/[id] and DELETE api method in /api/campground/[id]

--- pages/api/campground/[id].js
+++ pages/api/campground/[id].js
@@ -38,6 +38,21 @@ export default async (req, res) => {
 				res.status(400).json({ success: false });
 			}
 			break;
+		case 'DELETE':
+			try {
+				const campground = await db.collection('campgrounds').deleteOne(
+					{ _id: new ObjectID(id) },
+					{ $set: { title: req.body.title, location: req.body.location } },
+					{
+						new: true,
+					}
+				);
+
+				res.json(campground);
+			} catch (error) {
+				res.status(400).json({ success: false });
+			}
+			break;
 		default:
 			res.setHeader('Allow', ['GET', 'PUT']);
 			res.status(405).end(`Method ${method} Not Allowed`);
--- pages/campground/[id]/index.js
+++ pages/campground/[id]/index.js
@@ -1,4 +1,5 @@
 import Link from 'next/link';
+import axios from 'axios';
 
 import styles from '../../../styles/campground.module.css';
 
@@ -9,6 +10,21 @@ const fetcher = (url) => fetch(url).then((res) => res.json());
 
 const campground = () => {
 	const router = useRouter();
+
+	const handleDelete = (e) => {
+		e.preventDefault();
+
+		axios({
+			method: 'delete',
+			url: `/api/campground/${router.query.id}`,
+			data: {
+				_id: router.query.id,
+				location: data[0].location,
+				title: data[0].title,
+			},
+		});
+		router.push('/campgrounds');
+	};
 
 	const { data, error } = useSwr(
 		router.query.id ? `/api/campground/${router.query.id}` : null,
@@ -37,6 +53,11 @@ const campground = () => {
 						<a>Edit</a>
 					</Link>
 				</button>
+			</div>
+			<div>
+				<form onSubmit={handleDelete}>
+					<button>Delete</button>
+				</form>
 			</div>
 		</>
 	);
