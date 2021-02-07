81bcb190873f9f8c0aa0b6f9124212ca7247431c
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-02-06 19:45:10 -0600

    footer is now fixed to the bottom of the screen

--- components/layout/index.js
+++ components/layout/index.js
@@ -1,12 +1,13 @@
 import NavigationBar from '../Navbar';
 import Footer from '../Footer';
 
-export default function MyLayout({ children }) {
+export default function Layout({ children }) {
 	return (
 		<>
-			<NavigationBar />
-			{children}
-			<Footer />
+			<div className="d-flex flex-column vh-100">
+				<NavigationBar />
+				{children} <Footer />
+			</div>
 		</>
 	);
 }
