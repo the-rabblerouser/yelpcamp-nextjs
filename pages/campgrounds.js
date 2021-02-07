830d7195ef8e0930dd67882a83c9fee47a71da9f
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-02-07 14:51:32 -0600

    Card created for each campground in campgrounds list on campgrounds.js'

--- pages/campgrounds.js
+++ pages/campgrounds.js
@@ -1,6 +1,17 @@
 import Link from 'next/link';
 import styles from '../styles/campgrounds.module.css';
 import Layout from '../components/layout';
+import {
+	Card,
+	CardImg,
+	CardText,
+	CardBody,
+	CardTitle,
+	CardSubtitle,
+	Button,
+	Row,
+	Col,
+} from 'reactstrap';
 
 import useSwr from 'swr';
 
@@ -17,15 +28,39 @@ function Campgrounds() {
 			<div>
 				<h1>Campgrounds</h1>
 			</div>
-			{data.map(({ _id, title }) => {
+			{data.map(({ _id, title, image, description, location }) => {
 				return (
-					<ul key={_id}>
-						<li className={styles.listItem}>
-							<Link href={`/campground/[$_id]`} as={`/campground/${_id}`}>
-								<a>{title}</a>
-							</Link>
-						</li>
-					</ul>
+					<div key={_id}>
+						<Card className="mb-3">
+							<Row>
+								<Col md="4">
+									<CardImg
+										className="image-fluid"
+										width="100%"
+										src={image}
+										alt="Card image cap"
+									/>
+								</Col>
+								<Col md="8">
+									<CardBody>
+										<CardTitle tag="h5">{title}</CardTitle>
+										<CardSubtitle
+											tag="h6"
+											className="mb-2 text-muted"></CardSubtitle>
+										<CardText>{description}</CardText>
+										<CardSubtitle className="mb-4 text-muted">
+											{location}
+										</CardSubtitle>
+										<Link href={`/campground/[$_id]`} as={`/campground/${_id}`}>
+											<Button>
+												<a>View {title}</a>
+											</Button>
+										</Link>
+									</CardBody>
+								</Col>
+							</Row>
+						</Card>
+					</div>
 				);
 			})}
 		</>
