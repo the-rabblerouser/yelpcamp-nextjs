fa3dfb1bdd9d1bf36d376233458ac2c845429bae
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-02-10 23:55:37 -0600

    cleaned up newcampground.js old code

--- pages/NewCampground.js
+++ pages/NewCampground.js
@@ -1,45 +1,14 @@
-import React, { useReducer } from 'react';
 import { useRouter } from 'next/router';
 
 import axios from 'axios';
-import {
-	Button,
-	Row,
-	Col,
-	Form,
-	FormGroup,
-	Input,
-	InputGroup,
-	InputGroupAddon,
-} from 'reactstrap';
-import { useForm, Controller } from 'react-hook-form';
+
+import { useForm } from 'react-hook-form';
 
 import Layout from '../components/layout';
 // import style from '../styles/newcampground.module.css';
 
-// const initialState = {
-// 	title: '',
-// 	location: '',
-// 	description: '',
-// 	price: '',
-// 	image: '',
-// };
-
-// function reducer(state, { field, value }) {
-// 	return {
-// 		...state,
-// 		[field]: value,
-// 	};
-// }
-
 const NewCampground = () => {
-	// const [state, dispatch] = useReducer(reducer, initialState);
-
 	const router = useRouter();
-
-	// const handleChange = (e) => {
-	// 	dispatch({ field: e.target.name, value: e.target.value });
-	// };
 
 	const onSubmit = (data) => {
 		axios({
@@ -50,10 +19,10 @@ const NewCampground = () => {
 		router.push('/');
 	};
 
-	// const { title, location, description, price, image } = state;
+	// Console.log data to see the object data that is being submitted from the form
+	// console.log(data)
 
 	const { register, handleSubmit, errors } = useForm();
-	// const onSubmit = (data) => console.log(data);
 
 	return (
 		<>
@@ -118,64 +87,6 @@ const NewCampground = () => {
 					</form>
 				</div>
 			</div>
-			{/* <Row>
-				<h1 className="text-center mb-3">New Campground</h1>
-				<Col sm={{ size: 6, offset: 3 }}>
-					<Form onSubmit={handleSubmit(onSubmit)}>
-						<FormGroup className="mb-3">
-							<Input
-								type="text"
-								name="title"
-								placeholder="Title"
-								ref={register({ required: true })}
-							/>
-							{errors.title && 'Title is required'}
-						</FormGroup>
-						<FormGroup className="mb-3">
-							<Input
-								type="text"
-								name="location"
-								placeholder="Location"
-								value={location}
-								onChange={handleChange}
-							/>
-						</FormGroup>
-						<FormGroup className="mb-3">
-							<Input
-								type="textarea"
-								name="description"
-								placeholder="Description"
-								value={description}
-								onChange={handleChange}
-							/>
-						</FormGroup>
-						<FormGroup className="mb-3">
-							<InputGroup>
-								<InputGroupAddon addonType="prepend">
-									<span className="input-group-text">$</span>
-								</InputGroupAddon>
-								<Input
-									type="number"
-									name="price"
-									placeholder="Price"
-									value={price}
-									onChange={handleChange}
-								/>
-							</InputGroup>
-						</FormGroup>
-						<FormGroup className="mb-3">
-							<Input
-								type="text"
-								name="image"
-								placeholder="Image"
-								value={image}
-								onChange={handleChange}
-							/>
-						</FormGroup>
-						<Button color="dark">Add Campgrounds</Button>
-					</Form>
-				</Col>
-			</Row> */}
 		</>
 	);
 };
