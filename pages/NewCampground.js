24c64d9b3f7bf080d43cf64376b88f6a6e979635
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-02-10 23:51:02 -0600

    added react-hook-form in newcampground.js to handle client side form validation

--- pages/NewCampground.js
+++ pages/NewCampground.js
@@ -12,67 +12,124 @@ import {
 	InputGroup,
 	InputGroupAddon,
 } from 'reactstrap';
+import { useForm, Controller } from 'react-hook-form';
 
 import Layout from '../components/layout';
 // import style from '../styles/newcampground.module.css';
 
-const initialState = {
-	title: '',
-	location: '',
-	description: '',
-	price: '',
-	image: '',
-};
+// const initialState = {
+// 	title: '',
+// 	location: '',
+// 	description: '',
+// 	price: '',
+// 	image: '',
+// };
 
-function reducer(state, { field, value }) {
-	return {
-		...state,
-		[field]: value,
-	};
-}
+// function reducer(state, { field, value }) {
+// 	return {
+// 		...state,
+// 		[field]: value,
+// 	};
+// }
 
 const NewCampground = () => {
-	const [state, dispatch] = useReducer(reducer, initialState);
+	// const [state, dispatch] = useReducer(reducer, initialState);
 
 	const router = useRouter();
 
-	const handleChange = (e) => {
-		dispatch({ field: e.target.name, value: e.target.value });
-	};
+	// const handleChange = (e) => {
+	// 	dispatch({ field: e.target.name, value: e.target.value });
+	// };
 
-	const handleSubmit = (e) => {
-		e.preventDefault();
-
+	const onSubmit = (data) => {
 		axios({
 			method: 'post',
 			url: '/api/campgrounds',
-			data: {
-				title,
-				location,
-				description,
-				price,
-				image,
-			},
+			data,
 		});
 		router.push('/');
 	};
 
-	const { title, location, description, price, image } = state;
+	// const { title, location, description, price, image } = state;
+
+	const { register, handleSubmit, errors } = useForm();
+	// const onSubmit = (data) => console.log(data);
 
 	return (
 		<>
-			<Row>
+			<div className="row">
+				<h1 className="text-center mb-3">New Campground</h1>
+				<div className="col-md-6 offset-md-3">
+					<form onSubmit={handleSubmit(onSubmit)}>
+						<div className="mb-3">
+							<input
+								type="text"
+								name="title"
+								placeholder="Title"
+								className="form-control"
+								ref={register({ required: true })}
+							/>
+							{errors.title && '*Title is required'}
+						</div>
+						<div className="mb-3">
+							<input
+								name="location"
+								className="form-control"
+								type="text"
+								placeholder="Location"
+								ref={register({ required: true })}
+							/>
+							{errors.location && '*Location is required'}
+						</div>
+						<div className="mb-3">
+							<input
+								type="textarea"
+								name="description"
+								placeholder="Description"
+								className="form-control"
+								ref={register({ required: true })}
+							/>
+							{errors.description && '*Description is required'}
+						</div>
+						<div className="mb-3">
+							<div className="input-group">
+								<span className="input-group-text">$</span>
+								<input
+									type="number"
+									name="price"
+									placeholder="Price"
+									className="form-control"
+									ref={register({ required: true })}
+								/>
+							</div>
+							{errors.price && '* Price is required'}
+						</div>
+						<div className="mb-3">
+							<input
+								type="text"
+								name="image"
+								placeholder="Image"
+								className="form-control"
+								ref={register({ required: true })}
+							/>
+							{errors.image && '*Image URL is required'}
+						</div>
+						<button className="btn btn-dark">Add Campground</button>
+					</form>
+				</div>
+			</div>
+			{/* <Row>
 				<h1 className="text-center mb-3">New Campground</h1>
 				<Col sm={{ size: 6, offset: 3 }}>
-					<Form onSubmit={handleSubmit}>
+					<Form onSubmit={handleSubmit(onSubmit)}>
 						<FormGroup className="mb-3">
 							<Input
 								type="text"
 								name="title"
 								placeholder="Title"
-								value={title}
-								onChange={handleChange}
+								ref={register({ required: true })}
 							/>
+							{errors.title && 'Title is required'}
 						</FormGroup>
 						<FormGroup className="mb-3">
 							<Input
@@ -118,7 +175,7 @@ const NewCampground = () => {
 						<Button color="dark">Add Campgrounds</Button>
 					</Form>
 				</Col>
-			</Row>
+			</Row> */}
 		</>
 	);
 };
