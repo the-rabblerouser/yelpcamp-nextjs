5c2df6b3e43bfb4396f0a12b6cfb86de4bc377a2
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-03-10 09:57:11 -0600

    /api/[id]/review now uses Joi for validation on review

--- pages/api/campground/[id]/review.js
+++ pages/api/campground/[id]/review.js
@@ -1,4 +1,5 @@
 import nextConnect from 'next-connect';
+import Joi from 'joi';
 
 import dbConnect from '../../../../utils/mongodb';
 import Campground from '../../../../models/campground';
@@ -13,16 +14,28 @@ handler.post(async (req, res) => {
 		query: { id },
 	} = req;
 
+	const reviewSchema = Joi.object({
+		rating: Joi.string().required(),
+		body: Joi.string().required(),
+	}).required();
+
+	const { value, error } = reviewSchema.validate(req.body);
+
+	if (error) {
+		const msg = error.details
+			.map(({ message }) => `400 Bad Request: ${message}`)
+			.join(',');
+		return res.status(400).send(msg);
+	}
+
 	const campground = await Campground.findById(id);
 
-	const review = new Review(req.body);
+	const review = new Review(value);
 
 	campground.reviews.push(review);
 
 	await review.save();
 	await campground.save();
-
-	return res.end();
 });
 
 export default handler;
