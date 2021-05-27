bf7905b7e07e9b9608f023105864a8fc83a981ca
Joe McCann <joemccann@Joes-MacBook-Pro.local>
2021-05-26 22:40:27 -0500

    switched from bcrypt to crypto

--- pages/api/users.js
+++ pages/api/users.js
@@ -1,5 +1,5 @@
 import nextConnect from 'next-connect';
-import bcrypt from 'bcrypt';
+import crypto from 'crypto';
 
 import dbConnect from '../../utils/mongodb';
 import User from '../../models/user';
@@ -9,12 +9,16 @@ handler.post(async (req, res) => {
 	await dbConnect();
 
 	const { email, username, password } = req.body;
-	const hash = await bcrypt.hash(password, 12);
+	const salt = crypto.randomBytes(16).toString('hex');
+	const hash = crypto
+		.pbkdf2Sync(password, salt, 1000, 64, 'sha512')
+		.toString('hex');
 
 	const user = new User({
 		email,
 		username,
 		password: hash,
+		salt: salt,
 	});
 
 	await user.save();
